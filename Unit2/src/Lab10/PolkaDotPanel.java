b26471577bb643e128ee7e9bb87b3d842e5669a0
Mike <b11107026@gapps.ntust.edu.tw>
2026-05-07 16:04:46 +0800

    新增5/7 Ball、Polkadot、Pinball、Prize進度。刪除期末專案連結

--- Unit2/src/Lab10/PolkaDotPanel.java
+++ Unit2/src/Lab10/PolkaDotPanel.java
@@ -1,6 +1,6 @@
    //Torbert, e-mail: mr@torbert.com, website: www.mr.torbert.com
 	//version 6.17.2003
-
+   package Lab10;
    import javax.swing.*;
    import java.awt.*;
    import java.awt.event.*;
@@ -15,31 +15,35 @@
       private Graphics myBuffer;
       private Timer t;
       private Polkadot pd;
+      private Polkadot pd2;
       private int xPos, yPos;
-       public PolkaDotPanel()
-      {
+      public PolkaDotPanel(){
          myImage =  new BufferedImage(FRAME, FRAME, BufferedImage.TYPE_INT_RGB);
          myBuffer = myImage.getGraphics();
          myBuffer.setColor(BACKGROUND);
          myBuffer.fillRect(0, 0, FRAME, FRAME);
          pd = new Polkadot();
+         pd2 = new Polkadot(50, 50, 50, Color.BLUE);
          t = new Timer(1000, new Listener());
          t.start();
       }
-       public void paintComponent(Graphics g)
-      {
+      
+      public void paintComponent(Graphics g){
          g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
       }
        private class Listener implements ActionListener
       {
           public void actionPerformed(ActionEvent e)
          {
-         /**************************
-            your code goes here
-         **************************/
+            myBuffer.setColor(BACKGROUND);
+            myBuffer.fillRect(0, 0, FRAME, FRAME);
+
             pd.jump(FRAME, FRAME);
             pd.draw(myBuffer);
-            
+
+            pd2.jump(FRAME, FRAME);
+            pd2.draw(myBuffer);
+
             repaint();
          }
       }
--- Unit2/src/Lab11/Driver11.java
+++ Unit2/src/Lab11/Driver11.java
@@ -1,16 +1,16 @@
    //Torbert, e-mail: smtorbert@fcps.edu
 	//version 6.17.2003
-   package Lab10;
+   package Lab11;
    import javax.swing.JFrame;
-    public class Driver10
+    public class Driver11
    {
        public static void main(String[] args)
       { 
-         JFrame frame = new JFrame("Unit2, Lab10: Polka Dots");
+         JFrame frame = new JFrame("Unit2, Lab11");
          frame.setSize(400, 400);
-         frame.setLocation(0, 0);
+         frame.setLocation(200, 200);
          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       	frame.setContentPane(new PolkaDotPanel());
+       	frame.setContentPane(new PinballPanel());
          frame.setVisible(true);
       }
    }
